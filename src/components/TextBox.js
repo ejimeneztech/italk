70ada09698879c6a78fa30ce5d66e404feccfefb
Emilio Jimenez <emiliojimenez@Emilios-MBP.lan>
2023-03-01 14:49:28 -0800

    modified clear button so it only appears with text

--- src/components/TextBox.js
+++ src/components/TextBox.js
@@ -1,11 +1,14 @@
+import React, { useState } from "react";
 import "./TextBox.css";
 import ClearButton from "./ClearButton";
 
 export default function Textbox(props) {
+  const hasText = props.text;
+
   return (
     <div>
-      <div className="text-box">{props.text}</div>
-      <ClearButton onClick={props.clear} />
+      <div className="text-box">{hasText}</div>
+      {hasText && <ClearButton onClick={props.clear} />}
     </div>
   );
 }
