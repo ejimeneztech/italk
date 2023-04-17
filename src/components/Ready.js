dd9cb88a5cfb1a240aeac0905c47a1222d67a9fc
Emilio Jimenez <emiliojimenez@Emilios-MBP.lan>
2023-04-17 11:05:53 -0700

    added button filter

--- src/components/Ready.js
+++ src/components/Ready.js
@@ -4,14 +4,11 @@ import AudioPlayer from "./AudioPlayer";
 import Container from "./Container";
 import ButtonReady from "./ButtonReady";
 import ModalWrapper from "./ModalWrapper";
-import Section1 from "./Sections/Section1";
-import Section2 from "./Sections/Section2";
-import Section3 from "./Sections/Section3";
-
 
 export default function Ready(props) {
   const [text, setText] = useState("");
   const [audioSource, setAudioSource] = useState("");
+  const [activeCategory, setActiveCategory] = useState("activities");
 
   const onClickHandler = (event) => {
     const newAudio = event.target.dataset.value1;
@@ -31,43 +28,26 @@ export default function Ready(props) {
       <Textbox text={text} clear={clearButtonHandler} />
       <AudioPlayer source={audioSource} />
       <br></br>
-      <ModalWrapper refreshData={props.refreshData}/>
+      <ModalWrapper refreshData={props.refreshData} />
+      <br></br>
+      <button onClick={() => setActiveCategory("aboutMe")}>About Me</button>
+      <button onClick={() => setActiveCategory("neccessities")}>
+        Neccessities
+      </button>
+      <button onClick={() => setActiveCategory("activities")}>
+        Activities
+      </button>
+      <br></br>
       <Container>
-        <Section1>
-          {props.data.aboutMe.map((item) => (
-            <ButtonReady
-              onClick={onClickHandler}
-              value={item.text}
-              url={item.url}
-              img={item.img_url}
-              name={item.name}
-            />
-          ))}
-        </Section1>
-
-        <Section2>
-          {props.data.neccessities.map((item) => (
-            <ButtonReady
-              onClick={onClickHandler}
-              value={item.text}
-              url={item.url}
-              img={item.img_url}
-              name={item.name}
-            />
-          ))}
-        </Section2>
-
-        <Section3>
-          {props.data.activities.map((item) => (
-            <ButtonReady
-              onClick={onClickHandler}
-              value={item.text}
-              url={item.url}
-              img={item.img_url}
-              name={item.name}
-            />
-          ))}
-        </Section3>
+        {props.data[activeCategory].map((item) => (
+          <ButtonReady
+            onClick={onClickHandler}
+            value={item.text}
+            url={item.url}
+            img={item.img_url}
+            name={item.name}
+          />
+        ))}
       </Container>
     </div>
   );
