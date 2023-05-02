789bd7c9347d1ed37fec633053587b8df77827c3
Emilio Jimenez <emiliojimenez@Emilios-MBP.lan>
2023-05-02 14:41:03 -0700

    added stuff

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import './Navbar.css'; 
+import './Navbar.css';
 
+import profileIcon from "./images/profile.png"; // replace with the URL of your profile icon
 
 export default function Navbar() {
-    return (
-        <nav className="navbar">
-          <ul className="navbar-list">
-            <li className="navbar-item">iTalk</li>
-            
-          </ul>
-        </nav>
-      );
+  return (
+    <nav className="navbar">
+      <ul className="navbar-list">
+        <li className="navbar-item">iTalk</li>
+        {/* <li className="image-item">
+          <img className="profile-icon" src={profileIcon} alt="Profile" />
+        </li> */}
+      </ul>
+    </nav>
+  );
 }
