126b16e0574d83cc0ed3c708db1d53ae1d50bb58
Emilio Jimenez <emiliojimenez@Emilios-MBP.lan>
2023-05-04 14:35:12 -0700

    added new loading state

---
+++ src/components/ButtonLoading.js
@@ -0,0 +1,11 @@
+import "./ButtonLoading.css";
+
+
+
+export default function ButtonLoading() {
+    return (
+        <div className="load-button">
+          <div className="loading-animation"></div>
+        </div>
+      );
+}
--- src/components/NotReady.js
+++ src/components/NotReady.js
@@ -1,21 +1,19 @@
 import React from "react";
 import Textbox from "./TextBox";
-
 import Container from "./Container";
-
 import Filter from "./Filter";
+import ButtonLoading from "./ButtonLoading";
 
 export default function Ready() {
   return (
     <div>
       <br></br>
       <Textbox text="" />
-
       <br></br>
-
       <Filter />
-
-      <Container></Container>
+      <Container>
+        <ButtonLoading />
+      </Container>
       <br></br>
       <br></br>
     </div>
